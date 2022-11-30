8aa926e3140092afacaaff2df4e1053e5100840e
Abhijeet Sarkar <sarkar.abhi0512@gmail.com>
2022-11-30 14:21:30 +0530

    zond

--- beacon-chain/sync/subscriber_beacon_aggregate_proof_test.go
+++ beacon-chain/sync/subscriber_beacon_aggregate_proof_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/theQRL/zond/beacon-chain/operations/attestations"
 	lruwrpr "github.com/theQRL/zond/cache/lru"
 	fieldparams "github.com/theQRL/zond/config/fieldparams"
-	ethpb "github.com/theQRL/zond/proto/prysm/v1alpha1"
+	ethpb "github.com/theQRL/zond/protos/zond/v1alpha1"
 	"github.com/theQRL/zond/testing/assert"
 	"github.com/theQRL/zond/testing/require"
 	"github.com/theQRL/zond/testing/util"
